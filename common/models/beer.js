558a97afc76c348ce33999ee9d2f3ac23f13c87b
Eric Chou <eric@guard.ly>
2015-02-18 15:37:10 -0500

    add a new method to test destoryAll

--- common/models/beer.js
+++ common/models/beer.js
@@ -1,3 +1,27 @@
 module.exports = function(Beer) {
+	Beer.destroyAll = function(filter,cb) {
+		Beer.destroyAll();  //do not pass any parameters or callback function, will not be called
+
+
+		//cb(null, {"result":"good"});
+
+	};
+
+	Beer.remoteMethod(
+		'destroyAll', {
+			accepts: [{
+				arg: 'filter',
+				type: 'object'}],
+			returns: {
+				arg: 'mesg',
+				type: 'class'
+			},
+			http: {
+				path: '/',
+				verb: 'delete'
+			}
+		}
+	);
+
 
 };
